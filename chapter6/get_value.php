485b96ea32576e5994a9d1963ee7310d1814a5f8
mizuki-mimc <mizuki.o.110529@gmail.com>
2025-10-02 14:16:08 +0900

    add-PHP学習チャプター６

---
+++ chapter6/get_value.php
@@ -0,0 +1,11 @@
+<?php
+
+$associative_array = ["name" => "Alice", "age" => 30, "city" =>"Wonderland" ];
+
+print_r($associative_array);
+print_r($associative_array["name"]);
+echo PHP_EOL;
+print_r($associative_array["age"]);
+echo PHP_EOL;
+print_r($associative_array["city"]);
+echo PHP_EOL;
